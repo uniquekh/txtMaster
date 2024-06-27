6fc5ff213fac7f2c7a43d070323cb73186ac4687
Unique <mujeem000@gmail.com>
2024-06-27 11:49:18 +0530

    Update WEB_CONCURRENCY.sh

--- .profile.d/WEB_CONCURRENCY.sh
+++ .profile.d/WEB_CONCURRENCY.sh
@@ -1,26 +1,26 @@
 case $(ulimit -u) in
 
-# Automatic configuration for Gunicorn's Workers setting.
+Automatic configuration for Gunicorn's Workers setting.
 
-# Standard-1X (+Free, +Hobby) Dyno
+Standard-1X (+Free, +Hobby) Dyno
 256)
   export DYNO_RAM=512
   export WEB_CONCURRENCY=${WEB_CONCURRENCY:-2}
   ;;
 
-# Standard-2X Dyno
+Standard-2X Dyno
 512)
   export DYNO_RAM=1024
   export WEB_CONCURRENCY=${WEB_CONCURRENCY:-4}
   ;;
 
-# Performance-M Dyno
+Performance-M Dyno
 16384)
   export DYNO_RAM=2560
   export WEB_CONCURRENCY=${WEB_CONCURRENCY:-8}
   ;;
 
-# Performance-L Dyno
+Performance-L Dyno
 32768)
   export DYNO_RAM=14336
   export WEB_CONCURRENCY=${WEB_CONCURRENCY:-11}
